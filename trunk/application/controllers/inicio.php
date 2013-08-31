e8fe27539c83c26220a195ca8970c31b94ef9140
lodeale <lodeale@gmail.com>
2013-08-31 09:09:22 -0300

    Se quito la tarea

--- trunk/application/controllers/inicio.php
+++ trunk/application/controllers/inicio.php
@@ -1,15 +1,6 @@
 <?php
 
 class Inicio extends CI_Controller{
-	/*
-	* tarea:
-	* crear base de datos: libencode.
-	* Generar tabla usuarios:
-		campos: id_usuario, usuario, password, privilegio, activo
-	  Generar tabla persona:
-	  	campos: id_persona, nombre, apellido, direccion, provincia, tel, email,
-	  			dni.
-	*/
 
 	function __construct(){
 		parent::__construct();

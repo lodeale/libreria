c5b21c21ca7ea9eab4927417142ba07b1a625943
lodeale <lodeale@gmail.com>
2013-09-28 10:12:44 -0300

    Se actualizo la base de datos. Se agrego las tablas categoria y editorial. Se hicieron las relaciones

--- sql/libencode.sql
+++ sql/libencode.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 31-08-2013 a las 14:37:21
+-- Tiempo de generación: 28-09-2013 a las 10:11:43
 -- Versión del servidor: 5.1.66
--- Versión de PHP: 5.3.3-7+squeeze16
+-- Versión de PHP: 5.3.3-7+squeeze17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,6 +25,77 @@ USE `libencode`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE IF NOT EXISTS `autores` (
+  `id_autor` int(11) NOT NULL,
+  `nombre_apellido` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `editorial`
+--
+
+CREATE TABLE IF NOT EXISTS `editorial` (
+  `id_editorial` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(250) NOT NULL,
+  PRIMARY KEY (`id_editorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE IF NOT EXISTS `libros` (
+  `id_libro` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_editorial` int(11) NOT NULL,
+  `fecha_emision` date NOT NULL,
+  `pagina` int(5) NOT NULL,
+  `precio` decimal(7,2) NOT NULL,
+  `stock` int(8) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  PRIMARY KEY (`id_libro`),
+  KEY `id_categoria` (`id_categoria`),
+  KEY `id_editorial` (`id_editorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros_autores`
+--
+
+CREATE TABLE IF NOT EXISTS `libros_autores` (
+  `id_libro` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  KEY `id_libro` (`id_libro`),
+  KEY `id_autor` (`id_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `persona`
 --
 
@@ -38,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `persona` (
   `email` varchar(50) NOT NULL,
   `dni` varchar(12) NOT NULL,
   PRIMARY KEY (`id_persona`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -52,8 +123,34 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `password` varchar(140) NOT NULL,
   `privilegio` int(1) NOT NULL,
   `activo` int(1) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_persona` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_persona` (`id_persona`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD CONSTRAINT `libros_ibfk_3` FOREIGN KEY (`id_editorial`) REFERENCES `editorial` (`id_editorial`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `libros_autores`
+--
+ALTER TABLE `libros_autores`
+  ADD CONSTRAINT `libros_autores_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `libros_autores_ibfk_2` FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id_autor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

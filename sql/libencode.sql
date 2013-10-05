e9ac6aa04cc90e160952aafc199a67d1eabe3a47
lodeale <lodeale@gmail.com>
2013-10-05 10:13:03 -0300

    Se agregaron libros de pruebas pero completo

--- sql/libencode.sql
+++ sql/libencode.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-09-2013 a las 10:11:43
+-- Tiempo de generación: 05-10-2013 a las 10:12:28
 -- Versión del servidor: 5.1.66
 -- Versión de PHP: 5.3.3-7+squeeze17
 
@@ -29,10 +29,18 @@ USE `libencode`;
 --
 
 CREATE TABLE IF NOT EXISTS `autores` (
-  `id_autor` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_apellido` varchar(150) NOT NULL,
   PRIMARY KEY (`id_autor`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`id_autor`, `nombre_apellido`) VALUES
+(4, 'Friedrich Nietzsche'),
+(5, 'Alejandro Dolina');
 
 -- --------------------------------------------------------
 
@@ -44,7 +52,15 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(100) NOT NULL,
   PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `descripcion`) VALUES
+(4, 'filosofía'),
+(5, 'novelas');
 
 -- --------------------------------------------------------
 
@@ -56,7 +72,15 @@ CREATE TABLE IF NOT EXISTS `editorial` (
   `id_editorial` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(250) NOT NULL,
   PRIMARY KEY (`id_editorial`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `editorial`
+--
+
+INSERT INTO `editorial` (`id_editorial`, `descripcion`) VALUES
+(4, 'Centro Editorial de Cultura'),
+(5, 'Planeta');
 
 -- --------------------------------------------------------
 
@@ -78,7 +102,15 @@ CREATE TABLE IF NOT EXISTS `libros` (
   PRIMARY KEY (`id_libro`),
   KEY `id_categoria` (`id_categoria`),
   KEY `id_editorial` (`id_editorial`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id_libro`, `titulo`, `descripcion`, `id_categoria`, `id_editorial`, `fecha_emision`, `pagina`, `precio`, `stock`, `imagen`) VALUES
+(5, 'El anticristo', 'Friedrich wilhelm nietzsche, filósofo alemán, nacio en rocken , cerca de lutzen, en 1844 y murio en 1900, en weimar. Estudio primero en el colegi ode pforta y después filogoía clásica en la univerdades de bonn y leipzig; en esta ciujdad leyó por prim', 4, 4, '0000-00-00', 126, '10.00', 1, 'elanticristo.jpg'),
+(6, 'Cartas Marcadas', 'Cartas marcadas es un  libro envuelto en niebla. La cerrazón que cubre las clales de flores se tiende también sobre los capítulos de la novela provocando confusiones y obligándonos a marchar despacio. Por otra parte, la acción pervera de los conspira', 5, 5, '2012-05-05', 543, '150.00', 10, 'dolin.jpg');
 
 -- --------------------------------------------------------
 
@@ -92,6 +124,14 @@ CREATE TABLE IF NOT EXISTS `libros_autores` (
   KEY `id_libro` (`id_libro`),
   KEY `id_autor` (`id_autor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `libros_autores`
+--
+
+INSERT INTO `libros_autores` (`id_libro`, `id_autor`) VALUES
+(5, 4),
+(6, 5);
 
 -- --------------------------------------------------------
 
@@ -111,6 +151,13 @@ CREATE TABLE IF NOT EXISTS `persona` (
   PRIMARY KEY (`id_persona`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id_persona`, `nombre`, `apellido`, `direccion`, `provincia`, `tel`, `email`, `dni`) VALUES
+(1, 'admin', 'admin', 'pc', 'kernel', '234234', 'admin@admin.com', '223423423423');
+
 -- --------------------------------------------------------
 
 --
@@ -129,6 +176,13 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `password`, `privilegio`, `activo`, `id_persona`) VALUES
+(2, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1, 1, 1);
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -136,8 +190,8 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Filtros para la tabla `libros`
 --
 ALTER TABLE `libros`
-  ADD CONSTRAINT `libros_ibfk_3` FOREIGN KEY (`id_editorial`) REFERENCES `editorial` (`id_editorial`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `libros_ibfk_3` FOREIGN KEY (`id_editorial`) REFERENCES `editorial` (`id_editorial`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `libros_autores`

16d56b3c5174b0b4eba6bf91e74b2af3f01e978d
lodeale <lodeale@gmail.com>
2013-10-12 09:56:29 -0300

    Se agrego el view para ver la noticia seleccionada con los comentarios

---
+++ trunk/application/views/libros/libro.php
@@ -0,0 +1,84 @@
+ 		<div id="wrapper">
+			
+			<section class="viewLibro">
+				<article class="libroCompleto">
+					<h2>El titulo del libro Acá</h2>
+					<div>
+						<img src="<?php echo base_url(); ?>uploads/dolin.jpg" width="200" title="">
+					</div>
+					<div>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+						<br><br>
+						<h3>Detalle:</h3>
+						<ul class="lista1">
+							<li>Autor: Tanto</li>
+							<li>Categoria: tanto</li>
+							<li>Editorial: tanto</li>
+							<li>Pagin: 124</li>
+							<li>Stock: 1</li>
+							<li>Precio: $10</li>
+						</ul>
+					</div>
+				</article>
+			</section>
+			<section class="comentarios">
+				<div>
+					<ul>
+						<li>
+							<h3>Comentarios</h3>
+							<img src="<?php echo base_url(); ?>assets/img/mCli.png" width="150">
+							<input type="text" name="nombre" id="nombre" size="44" required placeholder="Ingrese su nombre..."><br>
+							<textarea name="comentario" id="comentario" cols="50" rows="5" placeholder="Ingrese un comentario..."></textarea>
+						</li>
+						<li>
+							<div>
+								<img src="<?php echo base_url(); ?>assets/img/mCli.png" width="50">
+								<h5>Fulano Miguel Angel</h5>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									sdflgksjdflgkjsdflgkjsldfkjglñdf
+								</p>
+							</div>
+						</li>
+						<li>
+							<div>
+								<img src="<?php echo base_url(); ?>assets/img/mCli.png" width="50">
+								<h5>Fulano Miguel Angel</h5>
+								<p>
+									Lorem ipsum liqua. Ut enim ad minim veniam,
+								</p>
+							</div>
+						</li>
+						<li>
+							<div>
+								<img src="<?php echo base_url(); ?>assets/img/mCli.png" width="50">
+								<h5>Fulano Miguel Angel</h5>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									
+								</p>
+							</div>
+						</li>
+						<li>
+							<div>
+								<img src="<?php echo base_url(); ?>assets/img/mCli.png" width="50">
+								<h5>Fulano Miguel Angel</h5>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>		
+			</section>
+			
+		</div>
